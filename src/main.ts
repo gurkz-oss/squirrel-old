3a232c259fe7a53b16a2969b7fa491046abc5198
Gurkan <hello@gurkz.me>
2023-09-20 16:53:33 +0200

    setup basic bot (does absolutely nothing)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("welcome to squirrel bot");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { env } from "./lib/env";
+
+const intents = [GatewayIntentBits.Guilds];
+
+const client = new Client({
+  intents,
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`ready as ${c.user.tag}`);
+});
+
+void client.login(env.TOKEN);
